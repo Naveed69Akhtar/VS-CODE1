436c5717206de1b6c2840ec001ef02fd2b951b57
Naveed Akhta <naveedakhtarnaveed69@gmail.com>
2023-02-22 09:46:16 -0800

    additionalHeaderFile

---
+++ c++Learning/additionalHeaderFile.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "naveed.h"
+using namespace std;
+int main()
+{
+    printStar(); // function declaration is in naveed.h file
+    printStar();
+    printStar();
+    return 0;
+}
---
+++ c++Learning/naveed.h
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+void printStar()
+{
+    cout << "***************************\n\n"; // this fuction can be used in verius files
+}
